1b0cf6a9a57210e7fa2d1284c0d8f3765b42206c
graciellenjose <graciellenascimentojose@gmail.com>
2025-06-28 13:32:49 -0300

    primeira aula

---
+++ src/br/com/basico/Basics.java
@@ -0,0 +1,10 @@
+package br.com.basico;
+
+import java.util.Scanner;
+
+public class Basics {
+    public static void main(String[] args){
+        //declaração de variáveis
+        Scanner scanner;
+    }
+}
